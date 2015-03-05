1aca2785f8d23cae5aa0f74da159743d43c9ae75
Eugene Sharygin <eush77@gmail.com>
2015-03-05 14:25:25 +0800

    Close #1 PR: Ignore return value in tests and usage examples.

--- test.js
+++ test.js
@@ -2,7 +2,21 @@
 var test = require('ava');
 var toFastProperties = require('./');
 
+var toSlowProperties = function (obj) {
+	for (var i = 0; i < 1000; ++i) {
+		obj['foo' + i] = 'foo';
+	}
+	return obj;
+};
+
 test(function (t) {
-	t.assert(%HasFastProperties(toFastProperties({})));
+	var obj = toSlowProperties({});
+	obj.foo = 'foo';
+	t.assert(!%HasFastProperties(obj), 'obj has slow properties');
+
+	toFastProperties(obj);
+	t.assert(%HasFastProperties(obj), 'obj has fast properties');
+	t.assert(obj.foo === 'foo', 'obj has the same keys');
+
 	t.end();
 });
