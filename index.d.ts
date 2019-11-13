a130c430372f7184fccbce65a11ff05d79fd4464
Sindre Sorhus <sindresorhus@gmail.com>
2019-11-13 11:43:31 +0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,9 @@
 /**
-Type definitions for to-fast-properties. Please use --esModuleInterop.
-*/
-/**
 Force V8 to use fast properties for an object.
 
-@param o Object to be made fast.
-@returns Shallow copy of the original object, but uncallable. Since the input is also made fast you should probably never use this.
-
-@remarks Callability should be declared stripped and reflected in tests when possible in the future.
+@param object - Object to be made fast.
+@returns A shallow copy of the original object, but uncallable.
 */
-declare function FastObject<T extends object>(o?: T | null): T;
-export = FastObject;
+declare function toFastProperties<T extends object>(object: T): T;
+
+export = toFastProperties;
